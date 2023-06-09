ce7580bdd06600032a16cff18f1d576a41e93a06
mrgeras <gerasimbek3@gmail.com>
2023-06-09 11:59:34 +0300

    База данных полностью, включая миграции, модели, сиды

---
+++ backend/db/models/city.js
@@ -0,0 +1,37 @@
+'use strict';
+const { Model } = require('sequelize');
+const myservice = require('./myservice');
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    static associate({ Country, myService }) {
+      this.belongsTo(Country, { foreignKey: 'country_id' });
+      this.hasMany(myService, { foreignKey: 'city_id' });
+    }
+  }
+  City.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      cityName: {
+        type: DataTypes.TEXT,
+      },
+      country_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Countries',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'City',
+    }
+  );
+  return City;
+};
