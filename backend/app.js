793b4f7e084a0456da9fe50e9b19552f674822b3
Alexxx2173 <Alexxx.Aksenov@gmail.com>
2023-06-09 14:56:59 +0300

    supabase create

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,18 @@
 require('@babel/register');
+require('dotenv').config();
+
 const express = require('express');
 const serverConfig = require('./config/serverConfig/serverConfig');
 
-
- const apiRouterPersonalisation = require('./routes/api/routes.personalisation');
-
-
+const apiRouterPersonalisation = require('./routes/api/routes.personalisation');
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
-
-
 serverConfig(app);
-
 
 app.use('/api', apiRouterPersonalisation);
 
-
-
-
 app.listen(PORT, () => {
-    console.log(`Go on port ${PORT}`)
-})
+  console.log(`Go on port ${PORT}`);
+});
