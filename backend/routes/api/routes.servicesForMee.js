b56957d981f26e9aa005c6256eb8dd0d0df6f81c
Alexxx2173 <Alexxx.Aksenov@gmail.com>
2023-06-13 18:34:57 +0300

    fix3

--- backend/routes/api/routes.servicesForMee.js
+++ backend/routes/api/routes.servicesForMee.js
@@ -1,28 +1,22 @@
 const router = require('express').Router();
 const { MyService, User, City, Service, MessChat } = require('../../db/models');
 
+router.route('/').get(async (req, res) => {
+  try {
+    const mySevices = await MyService.findAll({
+      include: [
+        { model: User },
+        { model: City },
 
-
-router.route('/')
-.get( async (req, res) => {
-try {
-    const mySevices = await MyService.findAll(
-        {
-        include:[
-            {model: User},
-            {model: City},
-
-            {model: Service},
-            {model: MessChat}
-
-        ]
-    }
-    )
-    res.json({mySevices})
-} catch (err) {
-    res.json({message: err.message})
-}
-
+        { model: Service },
+        { model: MessChat },
+      ],
+    });
+    res.json({ mySevices });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
