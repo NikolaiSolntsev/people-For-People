30406490f15e53949be6df995d56f47552d0e7c2
Alexxx2173 <Alexxx.Aksenov@gmail.com>
2023-06-16 13:57:56 +0300

    profilNew

--- frontend/src/components/account/items/AccountService.tsx
+++ frontend/src/components/account/items/AccountService.tsx
@@ -4,19 +4,16 @@ import { MyService } from '../../myService/type/MyService';
 function AccountService({ service }: { service: MyService }): JSX.Element {
   return (
     <div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '400px',
-          height: '400px',
-        }}
-      >
-        <img src={service.image} alt={service.description} />
-        <h1>{`location: ${service.City.cityName}`}</h1>
+      <div>
+        <img
+          style={{ maxWidth: '400px' }}
+          src={service.image}
+          alt={service.description}
+        />
+        {/* <h1>{`location: ${service.City.cityName}`}</h1>
         <h2>{`service: ${service.Service.serviceName}`}</h2>
         <h3>{`from user: ${service.User.name}`}</h3>
-        <h5>{`price: ${service.price}`}</h5>
+        <h5>{`price: ${service.price}`}</h5> */}
       </div>
     </div>
   );
